2dbd3712fcffab65733d02967fd697328d85b43b
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-09-18 17:02:00 -0400

    Implementada suma con delimitadores ',' y '-'

--- src/sumador_cadena.js
+++ src/sumador_cadena.js
@@ -1,10 +1,12 @@
+let array = [];
+let suma = 0;
 function sumar_cadena(cadena) {
   if (cadena.trim() == "")
   {
     return 0;
   }
-  let array = cadena.split(",");
-  let suma = 0;
+  array = cadena.split(/,|-/);
+  suma = 0;
   for (let i =0; i < array.length; i++)
   {
     suma += parseInt(array[i]);
