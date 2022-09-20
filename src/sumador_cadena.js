e561cd081c2592d86fd375e11affff41e8b9ac8e
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-09-19 21:51:28 -0400

    Se pueden pasar nuevos delimitadores mediante []

--- src/sumador_cadena.js
+++ src/sumador_cadena.js
@@ -1,17 +1,51 @@
 let array = [];
-let suma = 0;
+
+function concatenarDelimiters(regex)
+{
+  let str = ",|-"
+  if (regex != null)
+  {
+    str += "|" + regex;
+  }
+  return str;
+}
+
+function modificarStrDelimiter(str)
+{
+  str = str.replace(/\[|\]/g,"");
+  str = str.split(" ");
+  str = str.join("|");
+  return str;
+}
+
+
+function getDelimiters(cadena)
+{
+  let strDelimiters = cadena.match(/\[.*\]/g);
+  if (strDelimiters != null)
+  {
+    strDelimiters = modificarStrDelimiter(strDelimiters.toString());
+  }
+  let delimitadores = RegExp(concatenarDelimiters(strDelimiters));
+  return delimitadores;
+}
+
 function sumar_cadena(cadena) {
+  let suma = 0;
   if (cadena.trim() == "")
   {
     return 0;
   }
-  array = cadena.split(/,|-/);
-  suma = 0;
-  for (let i =0; i < array.length; i++)
+  let regex = getDelimiters(cadena);
+  cadena = cadena.replace(/\[.*\]/g,"");
+  array = cadena.split(regex);
+  for (let i = 0; i < array.length; i++)
   {
     suma += parseInt(array[i]);
   }
   return suma;
 }
+
+
 
 export default sumar_cadena;
--- src/sumador_cadena.spec.js
+++ src/sumador_cadena.spec.js
@@ -24,7 +24,13 @@ describe("Sumar cadena diferentes delimitadores", () => {
   it("Sumar una cadena con un delimitador ',' y un delimitador '-'", () => {
     expect(sumar_cadena("10,5-20")).toEqual(35);
   });
-  /*it("Sumar una cadena de varios valores con un delimitador de ,", () => {
-    expect(sumar_cadena("10,5,6,11")).toEqual(32);
-  });*/
+});
+
+describe("Sumar cadena con delimitadores personalizados", () => {
+  it("Sumar una cadena con un delimitador asignado", () => {
+    expect(sumar_cadena("[;]10;20")).toEqual(30);
+  });
+  it("Sumar una cadena con varios delimitador asignados", () => {
+    expect(sumar_cadena("[: ;]10:20;5-4,20")).toEqual(59);
+  });
 });
