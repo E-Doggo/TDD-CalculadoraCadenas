a80795e2f201a9d379b055f6f9847e571e9b8ccd
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-09-29 09:32:45 -0400

    Cohesion, nombres reemplazados, if inutilizado borrado, metodos privados

--- src/sumador_cadena.js
+++ src/sumador_cadena.js
@@ -1,55 +1,61 @@
-let array = [];
-
-function concatenarDelimiters(regex)
+class CalculadoraCadena
 {
-  let str = ",|-"
-  if (regex != null)
-  {
-    str += "|" + regex;
+
+  constructor(){
+    let arrayDeNumeros = [];
   }
-  return str;
-}
 
-function modificarStrDelimiter(str)
-{
-  str = str.replace(/\[|\]/g,"");
-  str = str.split(" ");
-  str = str.join("|");
-  return str;
-}
-
-
-function getDelimiters(cadena)
-{
-  let strDelimiters = cadena.match(/\[.*\]/g);
-  if (strDelimiters != null)
+  #concatenarDelimiters(strRegex)
   {
-    strDelimiters = modificarStrDelimiter(strDelimiters.toString());
-  }
-  let delimitadores = RegExp(concatenarDelimiters(strDelimiters));
-  return delimitadores;
-}
-
-function sumar_cadena(cadena) {
-  let suma = 0;
-  if (cadena.trim() == "")
-  {
-    return 0;
-  }
-  let regex = getDelimiters(cadena);
-  cadena = cadena.replace(/\[.*\]/g,"");
-  array = cadena.split(regex);
-  for (let i = 0; i < array.length; i++)
-  {
-    let num = parseInt(array[i]);
-    if (!isNaN(num) && num < 1000)
+    let str = ",|-"
+    if (strRegex != null)
     {
-      suma += num;
+      str += "|" + strRegex;
     }
+    return str;
   }
-  return suma;
+
+  #modificarDelimiter(str)
+  {
+    str = str.replace(/\[|\]/g,"");
+    str = str.split(" ");
+    str = str.join("|");
+    return str;
+  }
+
+
+  #getDelimiters(cadena)
+  {
+    let strDelimiters = cadena.match(/\[.*\]/g);
+    if (strDelimiters != null)
+    {
+      strDelimiters = this.#modificarDelimiter(strDelimiters.toString());
+    }
+    let delimitadores = RegExp(this.#concatenarDelimiters(strDelimiters));
+    return delimitadores;
+  }
+
+  #sumarArray()
+  {
+    let suma = 0;
+    for (let contador = 0; contador < this.arrayDeNumeros.length; contador++)
+    {
+      let num = parseInt(this.arrayDeNumeros[contador]);
+      if (!isNaN(num) && num < 1000)
+      {
+        suma += num;
+      }
+    }
+    return suma;
+  }
+
+  sumar(cadena) {
+    let regex = this.#getDelimiters(cadena);
+    cadena = cadena.replace(/\[.*\]/g,"");
+    this.arrayDeNumeros = cadena.split(regex);
+    let sum = this.#sumarArray();
+    return sum;
+  }
+
 }
-
-
-
-export default sumar_cadena;
+export default CalculadoraCadena;
--- src/sumador_cadena.spec.js
+++ src/sumador_cadena.spec.js
@@ -1,47 +1,72 @@
-import sumar_cadena from "./sumador_cadena.js"
+import CalculadoraCadena from "./sumador_cadena.js"
 
 describe("Devolver numeros", () => {
+  let calculadoraCadena = null;
+  beforeEach(() => {
+    calculadoraCadena = new CalculadoraCadena();
+  });
+
   it("Deberia mostrar el numero 0 cuando la cadena este vacia", () => {
-    expect(sumar_cadena(" ")).toEqual(0);
+    expect(calculadoraCadena.sumar(" ")).toEqual(0);
   });
 
   it("Deberia devolver un numero de una cadena", () => {
-    expect(sumar_cadena("1")).toEqual(1);
+    expect(calculadoraCadena.sumar("1")).toEqual(1);
   });
 });
 
 
 describe("Sumar cadena", () => {
+  let calculadoraCadena = null;
+  beforeEach(() => {
+    calculadoraCadena = new CalculadoraCadena();
+  });
   it("Sumar una cadena con un delimitador de ,", () => {
-    expect(sumar_cadena("10,5")).toEqual(15);
+    expect(calculadoraCadena.sumar("10,5")).toEqual(15);
   });
   it("Sumar una cadena de varios valores con un delimitador de ,", () => {
-    expect(sumar_cadena("10,5,6,11")).toEqual(32);
+    expect(calculadoraCadena.sumar("10,5,6,11")).toEqual(32);
   });
 });
 
 describe("Sumar cadena diferentes delimitadores", () => {
+  let calculadoraCadena = null;
+  beforeEach(() => {
+    calculadoraCadena = new CalculadoraCadena();
+  });
   it("Sumar una cadena con un delimitador ',' y un delimitador '-'", () => {
-    expect(sumar_cadena("10,5-20")).toEqual(35);
+    expect(calculadoraCadena.sumar("10,5-20")).toEqual(35);
   });
 });
 
 describe("Sumar cadena con delimitadores personalizados", () => {
+  let calculadoraCadena = null;
+  beforeEach(() => {
+    calculadoraCadena = new CalculadoraCadena();
+  });
+
   it("Sumar una cadena con un delimitador asignado", () => {
-    expect(sumar_cadena("[;]10;20")).toEqual(30);
+    expect(calculadoraCadena.sumar("[;]10;20")).toEqual(30);
   });
   it("Sumar una cadena con varios delimitador asignados", () => {
-    expect(sumar_cadena("[: ;]10:20;5-4,20")).toEqual(59);
+    expect(calculadoraCadena.sumar("[: ;]10:20;5-4,20")).toEqual(59);
   });
 
 
 });
 
 describe("Verificar valor", () => {
+  let calculadoraCadena = null;
+  beforeEach(() => {
+    calculadoraCadena = new CalculadoraCadena();
+  });
+
   it("No deberia aceptar la cadena porque no es numero", () => {
-    expect(sumar_cadena("cadena")).toEqual(0);
+    expect(calculadoraCadena.sumar("cadena")).toEqual(0);
   });
   it("No deberia tomar en cuenta el 1000", () => {
-    expect(sumar_cadena("1000,100,10")).toEqual(110);
+    expect(calculadoraCadena.sumar("1000,100,10")).toEqual(110);
   });
+
+  
 });
